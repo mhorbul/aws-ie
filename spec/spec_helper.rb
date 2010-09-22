a5e19cd11710bfb577ed051fbdd6a0ced70a877a
Efren Diaz <efren.diaz@sonian.net>
2010-09-23 03:04:31 +0800

    fix the specs to make the independent from the local timezone

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 
+# set the TZ env var to avoid Timezone conflicts when running the specs
+ENV["TZ"] = "UTC"
+
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
